7ff8e676efe6753e434b1ab3e0bf2bcef85492c0
RinkalAdesaraTops <167286606+RinkalAdesaraTops@users.noreply.github.com>
2024-05-20 09:05:26 +0530

    localstorage CRUD

--- Hooks/src/LocalstorageExample.jsx
+++ Hooks/src/LocalstorageExample.jsx
@@ -1,33 +1,22 @@
 import React,{useState} from 'react'
 
 const LocalstorageExample = () => {
-    const [name,setName] = useState("")
+    const [id,setId] = useState("")
     const [userData,setUserData] = useState([])
     const [data,setData] = useState({
         name:"",
-        age:""
+        age:"",
     })
+    // let allData = JSON.parse(localStorage.getItem("userData"))
     const handleChange = (e)=>{
         const {name,value} = e.target
 
         setData({
             ...data,
             [name]:value
-        })
-        // console.log(data);
-        
+        })        
     }
     const saveData = ()=>{
-        //JSON.stringify()
-        //JSON.parse()
-        // localStorage.setItem("name",name)
-        // console.log(data);
-        // userData.push(data)
-        // setUserData(prevData => [ // note that this is a square bracket (array), instead of a curly bracket (object)
-        //     ...prevData, // why not using the prev value
-        //     { data } // shorthand of { option: option }
-        // ]);
-        //to get prev data 
         setUserData(prevData => [
             ...prevData,
             data
@@ -35,35 +24,77 @@ const LocalstorageExample = () => {
         //latest added data newobject with name&age
         const updatedUserData = [...userData, data];
         localStorage.setItem("userData", JSON.stringify(updatedUserData));
-        // setUserData((prevData)=> (
-        //     [
-        //         ...prevData,
-        //         data
-        //     ]
-        // ))
-
-        // console.log("Userdata is -- ");
-        console.log(updatedUserData);
+        setUserData(updatedUserData)
+        setData({
+            name:"",
+            age:""
+        })
         
     }
-    const deleteData = ()=>{
-        //JSON.stringify()
-        //JSON.parse()
-        localStorage.removeItem("name")
+    const deleteData = (id)=>{
+        let allData = JSON.parse(localStorage.getItem("userData"))
+        let data1 = allData.filter((i,index)=>{
+                return index!=id
+        })
+        localStorage.setItem("userData",JSON.stringify(data1))
+        setUserData(data1)
+        
     }
-    const getData = ()=>{
-        //JSON.stringify()
-        //JSON.parse()
-        console.log(localStorage.getItem("name"))
+    const editData = (id)=>{
+        let allData = JSON.parse(localStorage.getItem("userData"))
+        let data1 = allData.filter((i,index)=>{
+                return index==id
+        })     
+        setData(data1[0])
+        setId(id)
     }
+    const updateData=()=>{
+        let allData = JSON.parse(localStorage.getItem("userData"))
+        let data1 = allData.map((i,index)=>{
+            if(index == id){
+                i.name = data.name
+                i.age = data.age
+            }
+            return i;
+        })
+        localStorage.setItem("userData",JSON.stringify(data1))
+        setUserData(data1)
+        setData({
+            name:"",
+            age:""
+        })
+        setId("")
+    }
+    
   return (
     <div>
-      <input type="text" name="name" id="name" value={data.name} onChange={handleChange}/>
-      <input type="text" name="age" id="age" value={data.age} onChange={handleChange}/><br /><br />
-      <input type="button" value="Save Data" onClick={saveData}/>
-      <input type="button" value="Delete Data" onClick={deleteData}/>
-      <input type="button" value="Get Data" onClick={getData}/>
+      Name : <input type="text" name="name" id="name" value={data.name} onChange={handleChange}/><br /><br />
+      Age : <input type="text" name="age" id="age" value={data.age} onChange={handleChange}/><br /><br />
+      <input type="button" value={id ? "Update Data": "Save Data"} onClick={id ? updateData : saveData}/> 
+      {/* <input type="button" value="Delete Data" /> */}
+      {/* <input type="button" value="Get Data" onClick={getData}/>   */}
 
+    <h3>All User List</h3>
+    <table>
+        <tr>
+            <td>Id</td>
+            <td>Name</td>
+            <td>Age</td>
+            <td>Action</td>
+        </tr>
+        {
+           userData && userData.map((i,index)=>{
+                return (
+                    <tr>
+                        <td>{index+1}</td>
+                        <td>{i.name}</td>
+                        <td>{i.age}</td>
+                        <td><button onClick={()=>editData(index)}>Edit</button><button onClick={()=>deleteData(index)}>Delete</button></td>
+                    </tr>
+                )
+           }) 
+        }
+    </table>
     </div>
   )
 }
